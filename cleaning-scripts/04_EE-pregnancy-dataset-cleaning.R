d10d9fcddb9715e1e16735904ccffc0cf95df9ec
Sophia Tan <sophiatan@berkeley.edu>
2021-07-08 12:21:37 -0700

    cleaning scripts

---
+++ cleaning-scripts/04_EE-pregnancy-dataset-cleaning.R
@@ -0,0 +1,108 @@
+
+
+
+rm(list=ls())
+
+source(here::here("0-config.R"))
+
+
+d<-box_read(830184329206)
+names(d)
+
+summary(d$mom_t0_ln_il2)
+
+######### Add combined ratios of cytokines ##########
+
+#drop Z-score, sd, and ratio measures
+d <- d[,!(grepl("^(z_)",colnames(d)) | grepl("^(sd_)",colnames(d)))]
+
+x=c("il1_mom_t0", "il6_mom_t0", "tnfa_mom_t0")[1]
+summary(as.vector(scale(d[,x], center = FALSE, scale = apply(as.matrix(d[,x]), 2, sd, na.rm = TRUE))))
+x=c("il1_mom_t0", "il6_mom_t0", "tnfa_mom_t0")[2]
+summary(as.vector(scale(d[,x], center = FALSE, scale = apply(as.matrix(d[,x]), 2, sd, na.rm = TRUE))))
+x=c("il1_mom_t0", "il6_mom_t0", "tnfa_mom_t0")[3]
+summary(as.vector(scale(d[,x], center = FALSE, scale = apply(as.matrix(d[,x]), 2, sd, na.rm = TRUE))))
+
+
+#function to create composite score
+create_score <- function(d, numerator_vars=c("il1_mom_t0", "il6_mom_t0", "tnfa_mom_t0"), denominator_vars="il10_mom_t0", varname="mom_t0_ratio_pro_il10"){
+  for(i in numerator_vars){
+    if(i==numerator_vars[1]){
+      x = as.vector(scale(d[,i], center = FALSE, scale = apply(as.matrix(d[,i]), 2, sd, na.rm = TRUE)))
+    }else{
+      x = x + as.vector(scale(d[,i], center = FALSE, scale = apply(as.matrix(d[,i]), 2, sd, na.rm = TRUE)))
+    }
+  }
+  summary(x)
+
+
+  for(i in denominator_vars){
+    if(i==denominator_vars[1]){
+      y = as.vector(scale(d[,i], center = FALSE, scale = apply(as.matrix(d[,i]), 2, sd, na.rm = TRUE)))
+    }else{
+      y = y + as.vector(scale(d[,i], center = FALSE, scale = apply(as.matrix(d[,i]), 2, sd, na.rm = TRUE)))
+    }
+  }
+  summary(y)
+
+  score=log(x/y)
+  summary(score)
+  d$score <- score
+  colnames(d)[ncol(d)] <- varname
+  return(d)
+}
+
+# *Pro-inflammatory cytokines / IL-10
+# *(IL-1 + IL-6 + TNF-a) / IL-10
+d <- create_score(d, numerator_vars=c("il1_mom_t0", "il6_mom_t0", "tnfa_mom_t0"), denominator_vars="il10_mom_t0", varname="mom_t0_ratio_pro_il10")
+summary(d$mom_t0_ratio_pro_il10)
+ggplot(d, aes(x=mom_t0_ratio_pro_il10)) + geom_density()
+
+# *Th1 / IL-10
+# *(IL-12 + IFN) / IL-10
+# gen mom_t0_ratio_th1_il10 = (il12_mom_t0 + ifng_mom_t0) / il10_mom_t0
+d <- create_score(d, numerator_vars=c("il12_mom_t0", "ifng_mom_t0"), denominator_vars="il10_mom_t0", varname="mom_t0_ratio_th1_il10")
+summary(d$mom_t0_ratio_th1_il10)
+ggplot(d, aes(x=mom_t0_ratio_th1_il10)) + geom_density()
+
+# *Th2 / IL-10
+# *(IL-4 + IL-5 + IL-13) / IL-10
+# gen mom_t0_ratio_th2_il10 = (il4_mom_t0 + il5_mom_t0 + il13_mom_t0) / il10_mom_t0
+d <- create_score(d, numerator_vars=c("il4_mom_t0", "il5_mom_t0", "il13_mom_t0"), denominator_vars="il10_mom_t0", varname="mom_t0_ratio_th2_il10")
+summary(d$mom_t0_ratio_th2_il10)
+ggplot(d, aes(x=mom_t0_ratio_th2_il10)) + geom_density()
+
+
+# *Th17 / IL-10
+# *(IL-17A + IL-21) / IL-10
+# gen mom_t0_ratio_th17_il10 = (il17_mom_t0 + il21_mom_t0) / il10_mom_t0
+d <- create_score(d, numerator_vars=c("il17_mom_t0", "il21_mom_t0"), denominator_vars="il10_mom_t0", varname="mom_t0_ratio_th17_il10")
+summary(d$mom_t0_ratio_th17_il10)
+ggplot(d, aes(x=mom_t0_ratio_th17_il10)) + geom_density()
+
+
+# *Th1 / Th2
+# *(IL-12 + IFN) / (IL-4 + IL-5 + IL-13)
+# gen mom_t0_ratio_th1_th2 = (il12_mom_t0 + ifng_mom_t0) / (il4_mom_t0 + il5_mom_t0 + il13_mom_t0)
+d <- create_score(d, numerator_vars=c("il12_mom_t0", "ifng_mom_t0"), denominator_vars=c("il4_mom_t0", "il5_mom_t0", "il13_mom_t0"), varname="mom_t0_ratio_th1_th2")
+summary(d$mom_t0_ratio_th1_th2)
+ggplot(d, aes(x=mom_t0_ratio_th1_th2)) + geom_density()
+
+
+# *Th1 / Th17
+# *(IL-12+IFN) / (IL-17A + IL-21)
+# gen mom_t0_ratio_th1_th17 = (il12_mom_t0 + ifng_mom_t0) / (il17_mom_t0 + il21_mom_t0)
+d <- create_score(d, numerator_vars=c("il12_mom_t0", "ifng_mom_t0"), denominator_vars=c("il17_mom_t0", "il21_mom_t0"), varname="mom_t0_ratio_th1_th17")
+summary(d$mom_t0_ratio_th1_th17)
+ggplot(d, aes(x=mom_t0_ratio_th1_th17)) + geom_density()
+
+
+########### Add deficiency cutoff exposures #############
+# 1 if deficient, 0 if not deficient
+d$vit_A_def <- ifelse(d$RBP_inf_preg < 0.83, 1, 0)
+d$vit_D_def <- ifelse(d$vitD_nmol_per_L < 30, 1, 0)
+d$iron_def <- ifelse(d$FERR_inf_preg < 12 | d$STFR_inf_preg > 8.3, 1, 0)
+
+
+box_write(d, "bangladesh-maternal-pregnancy-exposures-cleaned.RDS", dir_id = )
+
